fc34fe107f53e7302d4285a1fba82bc6db6e97d8
Haram Moon <harammoon@M1Pro.local>
2021-12-04 21:37:07 +0900

    Add create events test

---
+++ src/test/java/me/haram/demoinflearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,26 @@
+package me.haram.demoinflearnrestapi.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class EventControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                )
+                .andExpect(status().isCreated());
+    }
+}
