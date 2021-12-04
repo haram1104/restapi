629196f71d2f4dfcf01b656f942764cf9ba89e83
Haram Moon <harammoon@M1Pro.local>
2021-12-04 23:10:27 +0900

    POST /api/events returns 201

--- src/main/java/me/haram/demoinflearnrestapi/events/Event.java
+++ src/main/java/me/haram/demoinflearnrestapi/events/Event.java
@@ -2,17 +2,20 @@ package me.haram.demoinflearnrestapi.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
     private LocalDateTime closeEnrollmentDateTime;
-    private LocalDateTime beginEventTime;
+    private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
     private String location; // (optional) 이게 없으면 온라인 모임
     private int basePrice; // (optional)
@@ -20,5 +23,6 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus evensStatus;
 }
